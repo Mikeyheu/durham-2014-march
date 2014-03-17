829beac4310a24d06df57b3ae371dba59d424a64
Michael Heu <mheu@clickbooq.com>
2014-03-17 11:51:22 -0700

    fifth refactor using const_get to dynamically select VerseNum class

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,110 +9,23 @@ class Bottles
 	end
 
   def verse number
-  	VerseCompiler.new(number).to_s
-  end
-
-end
-
-class VerseCompiler
-	attr_reader :number
-
-	def initialize number
-		@number = number
-		case number
-		when 0,1,2
-			@verse = Object.const_get('VerseNum' + number.to_s).new(number)
-		else
-			@verse = VerseNum.new(number)
-		end
-	end
-
-	def to_s
-  	@verse.to_s
-  end
-
-end
-
-class VerseNum
-	attr_reader :number
-	def initialize number
-		@number = number
-	end
-
-	def to_s
-  	"#{current_inventory} #{current_container} #{liquid} #{location}, ".capitalize+
-  	"#{current_inventory} #{current_container} #{liquid}.\n"+
-  	"#{action}"+
-  	"#{next_inventory} #{next_container} #{liquid} #{location}.\n"
-  end
-
-  private
-
-  def location
-  	"on the wall"
-  end
-
-  def liquid
-  	"of beer"
-  end
-
-  def action 
-  	"Take #{pronoun(number)} down and pass it around, "
-  end
-
-  def current_inventory
-  	number
-  end
-
-  def next_inventory
-  	number-1
-  end
-
-  def pronoun number
-  	"one"
-  end
-
-  def current_container
-  	"bottles"
-  end
-
-  def next_container
-  	"bottles"
-  end
-  	
-  
-end
-
-class VerseNum0 < VerseNum
-	def action 
-  	"Go to the store and buy some more, "
-  end
-
-  def current_inventory
-  	"no more"
-  end
-
-  def next_inventory
-  	99
-  end
-end
-
-class VerseNum1 < VerseNum
-	def next_inventory
-		"no more"
-  end
-
-  def pronoun number
-  	"it"
-  end
-
-  def current_container
-  	"bottle"
-  end
-end
-
-class VerseNum2 < VerseNum
-  def next_container
-	  "bottle"
+  	case number
+  	when 0
+  		"No more bottles of beer on the wall, no more bottles of beer.\n"+
+	  	"Go to the store and buy some more, "+
+	  	"99 bottles of beer on the wall.\n"
+  	when 1
+  		"#{number} bottle of beer on the wall, #{number} bottle of beer.\n"+
+	  	"Take it down and pass it around, "+
+	  	"no more bottles of beer on the wall.\n"
+  	when 2
+  		"#{number} bottles of beer on the wall, #{number} bottles of beer.\n"+
+	  	"Take one down and pass it around, "+
+	  	"#{number-1} bottle of beer on the wall.\n"
+  	else
+	  	"#{number} bottles of beer on the wall, #{number} bottles of beer.\n"+
+	  	"Take one down and pass it around, "+
+	  	"#{number-1} bottles of beer on the wall.\n"
+	  end
   end
 end
