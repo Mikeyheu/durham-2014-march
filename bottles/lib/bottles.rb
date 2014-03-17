6d5f45940489393a920d26cebe53d96f83b78897
Michael Heu <mheu@clickbooq.com>
2014-03-17 10:38:48 -0700

    first refactor

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -11,21 +11,34 @@ class Bottles
   def verse number
   	case number
   	when 0
-  		"No more bottles of beer on the wall, no more bottles of beer.\n"+
+  		"No more bottles #{liquid} #{location}, no more bottles #{liquid}.\n"+
 	  	"Go to the store and buy some more, "+
-	  	"99 bottles of beer on the wall.\n"
+	  	"99 bottles #{liquid} #{location}.\n"
   	when 1
-  		"#{number} bottle of beer on the wall, #{number} bottle of beer.\n"+
+  		"#{number} bottle #{liquid} #{location}, #{number} bottle #{liquid}.\n"+
 	  	"Take it down and pass it around, "+
-	  	"no more bottles of beer on the wall.\n"
+	  	"no more bottles #{liquid} #{location}.\n"
   	when 2
-  		"#{number} bottles of beer on the wall, #{number} bottles of beer.\n"+
+  		"#{number} bottles #{liquid} #{location}, #{number} bottles #{liquid}.\n"+
 	  	"Take one down and pass it around, "+
-	  	"#{number-1} bottle of beer on the wall.\n"
+	  	"#{number-1} bottle #{liquid} #{location}.\n"
   	else
-	  	"#{number} bottles of beer on the wall, #{number} bottles of beer.\n"+
+	  	"#{number} bottles #{liquid} #{location}, #{number} bottles #{liquid}.\n"+
 	  	"Take one down and pass it around, "+
-	  	"#{number-1} bottles of beer on the wall.\n"
+	  	"#{number-1} bottles #{liquid} #{location}.\n"
 	  end
   end
+
+  private
+
+  def liquid
+  	"on the wall"
+  end
+
+  def location
+  	"of beer"
+  end
+
+
+
 end
